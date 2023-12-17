292e9bdbc9036169b1b315389f903e17102b2c7e
a.bakay1993 <a.bakay1993@gmail.com>
2023-12-17 03:16:35 +0300

    homeworkday3.2

--- Day_3/Homework/rect_magic_methods.py
+++ Day_3/Homework/rect_magic_methods.py
@@ -31,3 +31,59 @@ def __gt__(self, other):
 	# ...
 	# return True/False
 """
+
+class Rect:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def __repr__(self):
+        return f'Rectangle({self.length}, {self.width})'
+
+    def __str__(self):
+        return f'Rectangle with length {self.length} and width {self.width}'
+
+    def __mul__(self, arg):
+        if isinstance(arg, (int, float)):
+            return Rect(self.length * arg, self.width * arg)
+        else:
+            raise TypeError("Multiplier must be an int or float")
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+
+# Пример использования:
+r1 = Rect(2, 3)
+r2 = Rect(4, 5)
+
+print(repr(r1))  # Вывод: Rectangle(2, 3)
+print(str(r1))   # Вывод: Rectangle with length 2 and width 3
+
+r3 = r1 * 5
+print(repr(r3))  # Вывод: Rectangle(10, 15)
+
+print(r1 < r2)   # Вывод: True
+print(r1 == r2)  # Вывод: False
+print(r1 <= r2)  # Вывод: True
+print(r1 > r2)   # Вывод: False
+print(r1 >= r2)  # Вывод: False
+print(r1 != r2)  # Вывод: True
